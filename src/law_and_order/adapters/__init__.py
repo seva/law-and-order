38393098d733284ba15519b7986562b5fab1922e
seva <seva@users.noreply.github.com>
2026-08-30 02:04:22 -0400

    chore: erase everything except the constitution

--- src/law_and_order/adapters/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import PlatformAdapter, render_ruling
-from .discord import DiscordAdapter
-from .sim import SimAdapter
-
-__all__ = ["DiscordAdapter", "PlatformAdapter", "SimAdapter", "render_ruling"]
